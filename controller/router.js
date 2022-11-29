6056cce1c6ff71bc90072deba57a877252745381
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2022-11-29 13:37:27 +0700

    update login fail

--- controller/router.js
+++ controller/router.js
@@ -4,25 +4,33 @@ const qs = require('qs');
 
 class Router extends BaseController {
     static login = async (req, res) => {
-        if (req.method === 'GET') {
-        let dataHTML = await this.readFile('./view/login.html')
+        let dataHTML = await this.readFile('./view/login.html');
         res.writeHead(200, 'Content-Type', 'text/html');
         res.write(dataHTML);
+        res.end();
         }
-        else {
-            let data =''
-            req.on('data', chunk => {
-                data += chunk;
-            });
-            req.on('end', () => {
-                let user = qs.parse(data);
-                let checkUser = db.checkUser(user.email, user.password);
-                if (checkUser) {
-                    console.log('redirect');
-                    res.writeHead(301, {Location: './home'})
-                };
-            })
-        }
+    static submit_login = (req, res) => {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', async () => {
+            let user = qs.parse(data);
+            let checkUser = await db.checkUser(user.email, user.password);
+            if (checkUser) {
+                res.writeHead(301, {Location: './home'});
+            }
+            else {
+                res.writeHead(301, {Location: './login_fail'});
+            }
+            res.end();
+        });
+    }
+    static login_fail = async (req, res) => {
+        let dataHTML = await this.readFile('./view/login.html');
+        dataHTML = dataHTML.replace('<p class="text-danger"></p>', '<p class="text-danger">Incorrect email or password</p>');
+        res.writeHead(200, 'Content-Type', 'text/html');
+        res.write(dataHTML);
         res.end();
     }
     static home = async (req, res) => {
