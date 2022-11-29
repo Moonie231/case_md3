5cbf2d1234641035d662748aab6511c1bfedd8cc
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2022-11-29 10:04:04 +0700

    create server

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-function demo(){
-    console.log('Moonie')
-    console.log('hello');
-}
+const http = require('http');
+const url = require('url');
+const router = require('./controller/router.js');
+const db = require('./model/database.js')
+
+db.connect;
+
+const server = http.createServer((req, res) => {
+  let parseUrl = url.parse(req.url, true);
+  let path = parseUrl.pathname;
+  let trimPath = path.replace(/^\/+|\/$/g, '');
+  let handler = router[trimPath] ? router[trimPath] : router.notFound;
+  handler(req, res);
+});
+
+server.listen(8080, function () {
+  console.log('server running at localhost:8080');
+});
