5609cf659b2ccd8d40cf094374aaf546aaf90ba8
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2022-11-30 15:21:06 +0700

    split router into controller

---
+++ controller/userController.js
@@ -0,0 +1,57 @@
+const BaseController = require('./baseController.js');
+const db = require('../model/database.js');
+const qs = require('qs');
+const url = require('url');
+const navbar = require('../view/navbar.js');
+
+class User extends BaseController {
+    static logout = (req, res) => {
+        let cookie = qs.parse(req.headers.cookie);
+        let fileName = cookie.loginTime;
+        this.deleteSession(fileName);
+        res.writeHead(301, { Location: '/login' });
+        res.end();
+    }
+
+    static 'edit-info' = async (req, res) => {
+        let session = await this.getSessionData(req);
+        let dataHTML = await this.readFile('./view/user/edit-info.html');
+        let user = await db.getUser(session.email);
+        console.log(user);
+        dataHTML = dataHTML.replace('valueName', `value="${user.name}"`);
+        dataHTML = dataHTML.replace('valueBirthday', `value="${user.birthday}"`);
+        dataHTML = dataHTML.replace('valueEmail', `value="${user.email}"`);
+        dataHTML = dataHTML.replace('valueTelephone', `value="${user.telephone}"`);
+        dataHTML = dataHTML.replace('valueAvatar', `value="${user.avatar}"`);
+        dataHTML = dataHTML.replace('<nav></nav>', navbar);
+        res.writeHead(200, 'Content-Type', 'text/html');
+        res.write(dataHTML);
+        res.end();
+    }
+
+    static 'save-info' = (req, res) => {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            let user = qs.parse(data);
+            console.log("info update");
+            console.log(user);
+            db.updateUserInfo(user.email, user.name, user.birthday, user.telephone, user.avatar);
+        });
+        console.log('info saved');
+        res.writeHead(301, { Location: '/user/edit-info' });
+        res.end();
+    }
+    
+    static logout = (req, res) => {
+        let cookie = qs.parse(req.headers.cookie);
+        let fileName = cookie.loginTime;
+        this.deleteSession(fileName);
+        res.writeHead(301, { Location: '/login' });
+        res.end();
+    }
+}
+
+module.exports = User;
